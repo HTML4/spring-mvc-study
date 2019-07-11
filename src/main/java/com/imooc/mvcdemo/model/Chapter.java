25723881000166a18c100f417a87d25c1e735618
jason <jason@healthydeer.com>
2019-07-11 23:35:17 +0800

    course view page

---
+++ src/main/java/com/imooc/mvcdemo/model/Chapter.java
@@ -0,0 +1,5 @@
+package com.imooc.mvcdemo.model;
+
+public class Chapter {
+
+}
---
+++ src/main/java/com/imooc/mvcdemo/service/CourseService.java
@@ -0,0 +1,11 @@
+package com.imooc.mvcdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	private String name;
+	public void add() {
+		System.out.println("courseService");
+	}
+}
